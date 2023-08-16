716d5c080291183fbe10f41d2679fc67dfd5234a
Egor Pilipenko <pilipenko0409@gmail.com>
2023-08-17 01:11:31 +0700

    Change system_ext display name to "System-EXT" -Looks better in my opinion

--- partition.cpp
+++ partition.cpp
@@ -484,8 +484,8 @@ bool TWPartition::Process_Fstab_Line(const char *fstab_line, bool Display_Error,
 			Can_Be_Wiped = false;
 			Make_Dir(PartitionManager.Get_Android_Root_Path(), true);
 		} else if (Mount_Point == "/system_ext") {
-			Display_Name = "System_EXT";
-			Backup_Name = "System_EXT";
+			Display_Name = "System-EXT";
+			Backup_Name = "System-EXT";
 			Backup_Display_Name = Display_Name;
 			Storage_Name = Display_Name;
 			Can_Be_Backed_Up = Wipe_Available_in_GUI = Is_Super ? false : true;
