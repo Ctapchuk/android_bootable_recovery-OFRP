6a21d34ca0f7d04be78ae73ca6eb36b1a62d12db
micky387 <mickaelsaibi@free.fr>
2023-09-18 21:30:30 +0100

    gui.cpp: Only report touchscreen input when screen is on

--- gui/gui.cpp
+++ gui/gui.cpp
@@ -241,9 +241,14 @@ bool InputHandler::processInput(int timeout_ms)
 		break;
 	}
 
+#ifndef TW_NO_SCREEN_BLANK
+	if (!blankTimer.isScreenOff()) {
+#endif
 	if (ev.code != KEY_POWER && ev.code > KEY_RESERVED)
 		blankTimer.resetTimerAndUnblank();
-
+#ifndef TW_NO_SCREEN_BLANK
+	}
+#endif
 	return true;  // we got an event, so there might be more in the queue
 }
 
