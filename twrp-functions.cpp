075d1f4e6a2c79d840aaa0212649a2a3e6c1c84a
Egor Pilipenko <pilipenko0409@gmail.com>
2023-12-17 22:56:41 +0700

    Fix repack recovery for VAB with recovery Some VAB devices now have separate recovery partition so use it when repacking recovery
    Signed-off-by: Egor Pilipenko <ctapchuk@gmail.com>

--- twrp-functions.cpp
+++ twrp-functions.cpp
@@ -2940,20 +2940,20 @@ bool TWFunc::PackRepackImage_MagiskBoot(bool do_unpack, bool is_boot)
      }
  */
   TWPartition *Boot = PartitionManager.Find_Partition_By_Path("/boot");
-
-#if defined(AB_OTA_UPDATER) || defined(FOX_AB_DEVICE)
   if (Boot != NULL)
-    {
        tmpstr = Boot->Actual_Block_Device;
-#else 
+
   TWPartition *Recovery = PartitionManager.Find_Partition_By_Path("/recovery");
   if (Boot != NULL && Recovery != NULL)
     {
-      if (is_boot)
-	tmpstr = Boot->Actual_Block_Device;
-      else
-	tmpstr = Recovery->Actual_Block_Device;
-#endif
+       if (is_boot)
+	 tmpstr = Boot->Actual_Block_Device;
+       else
+	 tmpstr = Recovery->Actual_Block_Device;
+    }
+
+  if (tmpstr != "")
+    {
       if (do_unpack) // unpack
 	{
 	  if (TWFunc::Path_Exists(Fox_tmp_dir))
