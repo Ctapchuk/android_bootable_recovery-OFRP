9bb9507ab832e48b40c6f32b2e2ef4c15799e48b
Egor Pilipenko <ctapchuk@gmail.com>
2024-04-07 20:05:54 +0700

    exclude: Do not count size for excluded files If we add FILE to exclude from backup, Get_Folder_Size skips size count only for excluded dirs, not for FILE
    Signed-off-by: Egor Pilipenko <ctapchuk@gmail.com>

--- exclude.cpp
+++ exclude.cpp
@@ -93,7 +93,7 @@ uint64_t TWExclude::Get_Folder_Size(const string& Path) {
 		}
 		if ((st.st_mode & S_IFDIR) && !check_skip_dirs(FullPath) && de->d_type != DT_SOCK) {
 			dusize += Get_Folder_Size(FullPath);
-		} else if (st.st_mode & S_IFREG || st.st_mode & S_IFLNK) {
+		} else if ((st.st_mode & S_IFREG || st.st_mode & S_IFLNK) && !check_skip_dirs(FullPath)) {
 			dusize += (uint64_t)(st.st_size);
 		}
 	}
