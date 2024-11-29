58facac50f81ba4f02995a8267576f491778e595
Egor Pilipenko <ctapchuk@gmail.com>
2024-11-29 18:39:59 +0700

    gui: action.cpp: Toggle ADB before sideloading Should helps with broken sideloading mode when in sideload adb detects as recovery, not as sideload
    Signed-off-by: Egor Pilipenko <ctapchuk@gmail.com>

--- gui/action.cpp
+++ gui/action.cpp
@@ -2136,6 +2136,21 @@ int GUIAction::adbsideload(std::string arg __unused)
 		operation_end(0);
 	} else {
 		gui_msg("start_sideload=Starting ADB sideload feature...");
+
+		int adb_enabled = 0;
+		DataManager::GetValue("fox_adb", adb_enabled);
+		if (adb_enabled) {
+			property_set("ctl.stop", "adbd");
+			usleep(500000);
+			property_set("ctl.start", "adbd");
+			usleep(500000);
+		} else {
+			property_set("ctl.start", "adbd");
+			property_set("orangefox.adb.status", "1");
+			DataManager::SetValue("fox_adb", "1");
+			usleep(500000);
+		}
+
 		bool mtp_was_enabled = TWFunc::Toggle_MTP(false);
 
 		// wait for the adb connection
@@ -2158,6 +2173,12 @@ int GUIAction::adbsideload(std::string arg __unused)
 				PartitionManager.Wipe_Dalvik_Cache();
 		}
 		TWFunc::Toggle_MTP(mtp_was_enabled);
+		if (!adb_enabled) {
+			property_set("ctl.stop", "adbd");
+			property_set("orangefox.adb.status", "0");
+			DataManager::SetValue("fox_adb", "0");
+		}
+
 		reinject_after_flash();
 		operation_end(ret);
 	}
